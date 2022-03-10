1ea92d39296129edb2a400b33a05efa2ac7a8d43
Rootul Patel <rootulp@gmail.com>
2022-03-10 12:01:16 -0500

    feat: display count on Next example

--- examples/starknet-react-next/src/pages/index.tsx
+++ examples/starknet-react-next/src/pages/index.tsx
@@ -17,11 +17,19 @@ const Home: NextPage = () => {
       <h2>Counter Contract</h2>
       <p>Address: {counter?.connectedTo}</p>
       <p>Value: {counterValue?.count}</p>
+      <p>
+        Count:{' '}
+        {typeof counterValue?.count === 'string' && hexadecimalToDecimal(counterValue?.count)}
+      </p>
       <IncrementCounter />
       <h2>Recent Transactions</h2>
       <TransactionList />
     </div>
   )
+}
+
+function hexadecimalToDecimal(hexString: string): number {
+  return parseInt(hexString, 16)
 }
 
 export default Home
