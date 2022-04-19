d2d3c54863b7bf2dba2fdf733393bd3def5ae496
Abraham Makovetsky <avimak@gmail.com>
2022-04-19 13:10:09 +0300

    fix: memo connectors to avoid multiple auto-connect attempts
    `connectors` are deps of an inner useEffect in the core lib,
    by using memo we can avoid multiple auto-connect attempts

--- examples/starknet-react-next/src/pages/_app.tsx
+++ examples/starknet-react-next/src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import type { AppProps } from 'next/app'
 import NextHead from 'next/head'
 import { InjectedConnector, StarknetProvider } from '@starknet-react/core'
+import { useMemo } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const connectors = [new InjectedConnector()]
+  const connectors = useMemo(() => [new InjectedConnector()], [])
 
   return (
     <StarknetProvider autoConnect connectors={connectors}>
