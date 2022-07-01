a85ec99e183b834c2c3f0a9c62328577a1498f13
Francesco Ceccon <francesco@ceccon.me>
2022-07-01 16:35:48 +0100

    fix: core library version in next example
    Added an execute hook and demo
    
    Clean

---
+++ examples/starknet-react-next/pages/multicall.tsx
@@ -0,0 +1,31 @@
+import { useStarknet } from '@starknet-react/core'
+import type { NextPage } from 'next'
+import { ConnectWallet } from '~/components/ConnectWallet'
+import { UserBalance } from './token'
+import { TransactionList } from '~/components/TransactionList'
+import { DepositToken } from '~/components/DepositToken'
+
+
+const MulticallPage: NextPage = () => {
+    const { account } = useStarknet()
+
+    if (!account) {
+        return (
+          <div>
+            <p>Connect Wallet</p>
+            <ConnectWallet />
+          </div>
+        )
+    }
+
+    return (
+        <div>
+            <p>Connected: {account}</p>
+                <UserBalance />
+                <DepositToken />
+                <TransactionList />
+        </div>
+    )
+}
+
+export default MulticallPage
