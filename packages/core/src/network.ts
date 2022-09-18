a0b5c24c9daa8354455feea69fde0b05d89e01f5
clementjuventin <clementjuventin@gmail.com>
2022-09-18 13:32:22 +0200

    feat: add useNetwork hook

---
+++ packages/core/src/network.ts
@@ -0,0 +1,40 @@
+import { StarknetChainId } from 'starknet/constants'
+
+export type BlockExplorerName = 'voyager'
+export type BlockExplorer = { name: string; url: string }
+
+/** Information about a StarkNet network. */
+export type Chain = {
+  /** Chain ID. */
+  id: string
+  /** Human-readable name. */
+  name: string
+  /** Block explorer. */
+  blockExplorer?: BlockExplorer
+  /** Flag for test networks. */
+  testnet?: boolean
+}
+
+export function chainById(chainId: StarknetChainId): Chain | undefined {
+  return KNOWN_CHAINS[chainId]
+}
+
+const KNOWN_CHAINS: Record<StarknetChainId, Chain> = {
+  [StarknetChainId.MAINNET]: {
+    id: StarknetChainId.MAINNET,
+    name: 'StarkNet Mainnet',
+    blockExplorer: {
+      name: 'Voyager',
+      url: 'https://voyager.online',
+    },
+    testnet: false,
+  },
+  [StarknetChainId.TESTNET]: {
+    id: StarknetChainId.TESTNET,
+    name: 'StarkNet Görli',
+    blockExplorer: {
+      name: 'Voyager',
+      url: 'https://goerli.voyager.online',
+    },
+  },
+}
