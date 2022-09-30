3b17b580fada6b2c782354774e8fe6d3cb163e75
Francesco Ceccon <francesco@ceccon.me>
2022-09-30 19:07:18 +0100

    chore: fix export basepath

--- website/components/Layout.tsx
+++ website/components/Layout.tsx
@@ -6,7 +6,9 @@ export function Layout({ children }: { children?: React.ReactNode }) {
   return (
     <Box h="100vh" display="flex" flexDir="column">
       <Header />
-      <Box flexGrow="1">{children}</Box>
+      <Box flexGrow="1" bg="cat.base">
+        {children}
+      </Box>
     </Box>
   )
 }
--- website/next.config.js
+++ website/next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: '/starknet-react',
+  assetPrefix: '/starknet-react',
 }
 
 module.exports = nextConfig
