6917c5c47a11de75f9a531bc425ae6216820c939
Francesco Ceccon <francesco@ceccon.me>
2022-09-30 10:21:44 +0100

    docs: update home

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Heading, HStack, VStack } from '@chakra-ui/react'
+import { Box, Button, Center, Heading, HStack, VStack } from '@chakra-ui/react'
 import React from 'react'
 import NextLink from 'next/link'
 import { Layout } from '../components/Layout'
@@ -6,28 +6,30 @@ import { Layout } from '../components/Layout'
 export default function IndexPage() {
   return (
     <Layout>
-      <VStack pt="20">
-        <Box textAlign="center">
-          <Heading as="h1" fontSize="7xl">
-            StarkNet React
-          </Heading>
-          <Heading as="h2" fontSize="4xl" color="cat.peach" mt="4">
-            A collection of React hooks for StarkNet.
-          </Heading>
-        </Box>
-        <HStack pt="8" gap="20">
-          <NextLink href="/get-started" passHref>
-            <Button size="lg" as="a" variant="outline" colorScheme="whiteAlpha">
-              Get Started
-            </Button>
-          </NextLink>
-          <NextLink href="/hooks" passHref>
-            <Button size="lg" as="a" variant="outline" colorScheme="whiteAlpha">
-              View Hooks
-            </Button>
-          </NextLink>
-        </HStack>
-      </VStack>
+      <Center h="full">
+        <VStack>
+          <Box textAlign="center">
+            <Heading as="h1" fontSize="7xl">
+              StarkNet React
+            </Heading>
+            <Heading as="h2" fontSize="4xl" color="cat.peach" mt="4">
+              A collection of React hooks for StarkNet.
+            </Heading>
+          </Box>
+          <HStack pt="8" gap="20">
+            <NextLink href="/get-started" passHref>
+              <Button size="lg" as="a" variant="outline" colorScheme="whiteAlpha">
+                Get Started
+              </Button>
+            </NextLink>
+            <NextLink href="/hooks" passHref>
+              <Button size="lg" as="a" variant="outline" colorScheme="whiteAlpha">
+                View Hooks
+              </Button>
+            </NextLink>
+          </HStack>
+        </VStack>
+      </Center>
     </Layout>
   )
 }
