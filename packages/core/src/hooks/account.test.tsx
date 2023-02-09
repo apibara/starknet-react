753e81d480b135f1c28a5bff0f8264a9b4424a34
Iris <iris.devillars@gmail.com>
2023-02-09 12:53:26 +0100

    fix: merge conflicts with wagmi

--- packages/core/src/hooks/account.test.tsx
+++ packages/core/src/hooks/account.test.tsx
@@ -7,16 +7,7 @@ import { Connector } from '~/connectors'
 
 function useTestHook() {
   const { connectors, connect } = useStarknet()
-  const {
-    account,
-    address,
-    connector,
-    status,
-    isConnected,
-    isConnecting,
-    isReconnecting,
-    isDisconnected,
-  } = useAccount()
+  const { account, address, connector, status } = useAccount()
   return {
     account,
     connectors,
@@ -24,10 +15,6 @@ function useTestHook() {
     address,
     connector,
     status,
-    isConnected,
-    isConnecting,
-    isReconnecting,
-    isDisconnected,
   }
 }
 
@@ -67,10 +54,6 @@ describe('useAccount', () => {
         expect(result.current.address).toBeUndefined()
         expect(result.current.connector).toBeUndefined()
         expect(result.current.status).toEqual('disconnected')
-        expect(result.current.isConnected).toBeFalsy()
-        expect(result.current.isConnecting).toBeFalsy()
-        expect(result.current.isReconnecting).toBeFalsy()
-        expect(result.current.isDisconnected).toBeTruthy()
       })
     })
   })
@@ -114,8 +97,6 @@ describe('useAccount', () => {
         expect(result.current.address).toEqual(deventAccounts[2].address)
         expect(result.current.connector).toBeDefined()
         expect(result.current.status).toEqual('connected')
-        expect(result.current.isConnected).toBeTruthy()
-        expect(result.current.isDisconnected).toBeFalsy()
       })
     })
   })
