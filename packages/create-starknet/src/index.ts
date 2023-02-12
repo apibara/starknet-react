2fa7c7d126b6801ec08e003e7a375b61edfc50b9
Yohan Tancrez <yohantancrez@gmail.com>
2023-02-12 18:08:30 +0100

    feat: created create-starknet script

---
+++ packages/create-starknet/src/index.ts
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+
+import path from 'path'
+import prompts from 'prompts'
+import validatePackageName from 'validate-npm-package-name'
+import fs from 'fs-extra'
+
+const handleSigTerm = () => process.exit(0)
+
+process.on('SIGINT', handleSigTerm)
+process.on('SIGTERM', handleSigTerm)
+
+const templatesFolderPath = path.join(__dirname, '..', 'templates')
+
+let projectPath = ''
+
+// TODO SETUP COMMANDER HERE
+
+async function run() {
+  const response = await prompts({
+    initial: 'my-starknet-app',
+    type: 'text',
+    name: 'projectPath',
+    message: 'What is your project named?',
+    validate: (projectPath) => {
+      const projectNameValidation = validatePackageName(path.basename(path.resolve(projectPath)))
+
+      if (!projectNameValidation.validForNewPackages) {
+        return [
+          ...(projectNameValidation.warnings || []),
+          ...(projectNameValidation.errors || []),
+        ].join('\n')
+      }
+
+      if (fs.existsSync(path.resolve(projectPath))) {
+        return 'A file with this name already exists'
+      }
+
+      return true
+    },
+  })
+
+  if (typeof response.projectPath === 'string') {
+    projectPath = response.projectPath.trim()
+  }
+
+  const resolvedProjectPath = path.resolve(projectPath)
+  const projectName = path.basename(resolvedProjectPath)
+
+  // TODO: Pick the proper template
+  const selectedTemplatePath = path.join(templatesFolderPath, 'next')
+
+  fs.copySync(selectedTemplatePath, resolvedProjectPath, { overwrite: false })
+
+  const filesToRename = [
+    ['gitignore', '.gitignore'],
+    ['eslintrc.json', '.eslintrc.json'],
+    ['README-template.md', 'README.md'],
+  ]
+
+  filesToRename.forEach(([previousFileName, newFileName]) => {
+    const previousFilePath = path.join(resolvedProjectPath, previousFileName)
+    const newFilePath = path.join(resolvedProjectPath, newFileName)
+
+    if (fs.existsSync(previousFilePath)) {
+      fs.renameSync(previousFilePath, newFilePath)
+    }
+  })
+
+  const packageJsonPath = path.join(resolvedProjectPath, 'package.json')
+  const packageJson = fs.readJsonSync(packageJsonPath)
+  packageJson.name = projectName
+
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
+}
+
+run()
---
+++ packages/create-starknet/templates/next/pages/_app.js
@@ -0,0 +1,7 @@
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+require('@/styles/globals.css')
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+exports.default = App
