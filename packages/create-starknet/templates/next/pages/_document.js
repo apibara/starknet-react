4906703a028876ce3baac289d3ac18346f2279c6
Yohan Tancrez <yohantancrez@gmail.com>
2023-02-12 18:08:30 +0100

    feat: created create-starknet script

---
+++ packages/create-starknet/templates/next/pages/_document.js
@@ -0,0 +1,15 @@
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+const document_1 = require('next/document')
+function Document() {
+  return (
+    <document_1.Html lang="en">
+      <document_1.Head />
+      <body>
+        <document_1.Main />
+        <document_1.NextScript />
+      </body>
+    </document_1.Html>
+  )
+}
+exports.default = Document
