883c1fcb4f8c97994845ecf43aa47d6321f351f8
Yohan Tancrez <yohantancrez@gmail.com>
2023-02-14 00:28:12 +0100

    chore(starknet-react): deleted unused files and took PR comments into account

--- packages/create-starknet/templates/next/pages/_app.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-require('@/styles/globals.css')
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-exports.default = App
