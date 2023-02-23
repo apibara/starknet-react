9a2955d9f5863415488c18a297d867b6c6387140
Yohan Tancrez <yohantancrez@gmail.com>
2023-02-23 16:24:38 +0100

    feat(create-starknet): integrated starknet-react to the next template

---
+++ packages/create-starknet/templates/next/components/WalletBar.tsx
@@ -0,0 +1,42 @@
+import { useAccount, useConnectors } from '@starknet-react/core'
+import { useMemo } from 'react'
+
+function WalletConnected() {
+  const { address } = useAccount()
+  const { disconnect } = useConnectors()
+
+  const shortenedAddress = useMemo(() => {
+    if (!address) return ''
+    return `${address.slice(0, 6)}...${address.slice(-4)}`
+  }, [address])
+
+  return (
+    <div>
+      <span>Connected: {shortenedAddress}</span>
+      <button onClick={disconnect}>Disconnect</button>
+    </div>
+  )
+}
+
+function ConnectWallet() {
+  const { connectors, connect } = useConnectors()
+
+  return (
+    <div>
+      <span>Choose a wallet:</span>
+      {connectors.map((connector) => {
+        return (
+          <button key={connector.id()} onClick={() => connect(connector)}>
+            {connector.id()}
+          </button>
+        )
+      })}
+    </div>
+  )
+}
+
+export default function WalletBar() {
+  const { address } = useAccount()
+
+  return address ? <WalletConnected /> : <ConnectWallet />
+}
--- packages/create-starknet/templates/next/pages/_app.tsx
+++ packages/create-starknet/templates/next/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
+import { InjectedConnector, StarknetConfig } from '@starknet-react/core'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const connectors = [
+    new InjectedConnector({ options: { id: 'braavos' } }),
+    new InjectedConnector({ options: { id: 'argentX' } }),
+  ]
+
+  return (
+    <StarknetConfig autoConnect connectors={connectors}>
+      <Component {...pageProps} />
+    </StarknetConfig>
+  )
 }
