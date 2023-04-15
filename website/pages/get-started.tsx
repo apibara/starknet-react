9b874a4e14a59e1df5a76469d61cfd5d9027d321
Yohan Tancrez <yohantancrez@gmail.com>
2023-04-15 02:24:47 +0200

    docs: updated get started page to suggest create-starknet

--- website/pages/get-started.tsx
+++ website/pages/get-started.tsx
@@ -5,6 +5,22 @@ import { Layout } from '../components/Layout'
 import { Markdown } from '../components/Markdown'
 import { Section, SectionHeading } from '../components/Section'
 
+const CREATE_STARKNET_HELP = `
+  \`\`\`bash
+  Usage: create-starknet [project-directory] [options]
+
+  Create starknet apps with one command
+  
+  Options:
+    -V, --version          output the version number
+    -t, --template <name>  Explicitly tell the CLI to bootstrap the app using the specified template (choices: "next", "vite")
+    --use-npm              Explicitly tell the CLI to bootstrap the app using npm
+    --use-yarn             Explicitly tell the CLI to bootstrap the app using yarn
+    --use-pnpm             Explicitly tell the CLI to bootstrap the app using pnpm
+    -h, --help             display help for command  
+  \`\`\`
+`
+
 const PROVIDER_IMPORT = `
 \`\`\`ts
 import { StarknetConfig, InjectedConnector } from '@starknet-react/core'
@@ -40,7 +56,40 @@ export default function GetStartedPage() {
           Start building StarkNet applications in less than a minute.
         </Heading>
         <Section>
-          <SectionHeading>Installation</SectionHeading>
+          <SectionHeading>Quick setup</SectionHeading>
+          <Text mt="10">
+            We recommend creating a new starknet-react app using create-starknet, which sets up a
+            starknet app using TypeScript automatically for you. To create a project, run:
+          </Text>
+          <Tabs colorScheme="whiteAlpha" variant="soft-rounded" mt="4">
+            <TabList>
+              <Tab>pnpm</Tab>
+              <Tab>yarn</Tab>
+              <Tab>npm</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Markdown>`pnpm create starknet`</Markdown>
+              </TabPanel>
+              <TabPanel>
+                <Markdown>`yarn create starknet`</Markdown>
+              </TabPanel>
+              <TabPanel>
+                <Markdown>`npx create-starknet`</Markdown>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+          <Text mt="4">
+            You can also pass command line arguments to set up a new project non-interactively. See
+            create-starknet --help:
+          </Text>
+          <Markdown>{CREATE_STARKNET_HELP}</Markdown>
+        </Section>
+        <Section>
+          <SectionHeading>Manual setup</SectionHeading>
+          <Heading size="lg" mb="4" mt="10">
+            Installation
+          </Heading>
           <Text>Add `@starknet-react/core` using your favorite package manager.</Text>
           <Tabs colorScheme="whiteAlpha" variant="soft-rounded" mt="4">
             <TabList>
@@ -60,9 +109,9 @@ export default function GetStartedPage() {
               </TabPanel>
             </TabPanels>
           </Tabs>
-        </Section>
-        <Section>
-          <SectionHeading>Usage with Next.js</SectionHeading>
+          <Heading size="lg" mb="4" mt="10">
+            Usage with Next.js
+          </Heading>
           <Text>Start by adding the following import statement to your `_app.tsx` file.</Text>
           <Markdown>{PROVIDER_IMPORT}</Markdown>
           <Text>Then edit the application component to include the StarkNet provider.</Text>
