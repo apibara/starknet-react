32bb2bb79b571b7b60970100c2336ca605057d58
David Lange <lange@finiam.com>
2023-05-25 18:38:56 +0100

    fix: useContractRead broken code example

--- packages/core/src/hooks/call.ts
+++ packages/core/src/hooks/call.ts
@@ -63,7 +63,7 @@ export interface UseContractReadResult {
  * ```tsx
  * function Component() {
  *   const { address } = useAccount()
- *   const { data, isLoading, error, refetch } = useStarknetCall({
+ *   const { data, isLoading, error, refetch } = useContractRead({
  *     address: ethAddress,
  *     abi: compiledErc20.abi,
  *     functionName: 'balanceOf',
@@ -72,7 +72,7 @@ export interface UseContractReadResult {
  *   })
  *
  *   if (isLoading) return <span>Loading...</span>
- *   if (error) return <span>Error: {error}</span>
+ *   if (error) return <span>Error: {JSON.stringify(error)}</span>
  *
  *   return (
  *     <div>
