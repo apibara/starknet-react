a0fd77c98ec11a81ad6658d916c5f040a35be6c3
Francesco Ceccon <francesco@ceccon.me>
2023-05-25 22:11:30 +0100

    fix: export dynamic paths in website

--- website/app/hooks/[name]/page.tsx
+++ website/app/hooks/[name]/page.tsx
@@ -12,3 +12,8 @@ export default function Page({ params }: { params: IParams }) {
 
   return <Hooks hook={hook} />
 }
+
+export async function generateStaticParams() {
+  const hooks = getAllHooks()
+  return hooks.map(({ name }) => ({ name }))
+}
