30842775b611111add458790f59fa28e99f88481
Laki997 <lazar_kalajdzic@epam.com>
2023-05-30 18:52:57 +0200

    feat: align code block text to left

--- website/components/Hooks.tsx
+++ website/components/Hooks.tsx
@@ -5,7 +5,7 @@ import { Hook } from '../components/Hook'
 
 export default function Hooks({ hook }: { hook: any }) {
   return (
-    <div className=" min-h-screen mx-4 lg:mx-0 flex-col items-center text-cat-text my-0 pt-12 mb-20 text-center ">
+    <div className=" min-h-screen mx-4 lg:mx-0 flex-col items-center text-cat-text my-0 pt-12 mb-20">
       <div className=" text-center sm:text-4xl md:text-5xl lg:text-6xl md:text-center lg:text-center text-2xl  sm:text-center">
         {hook?.name}
       </div>
