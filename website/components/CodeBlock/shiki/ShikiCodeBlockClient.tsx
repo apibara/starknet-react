1865115e7dddead114445bd282f260ad51cd7695
Laki997 <lazar_kalajdzic@epam.com>
2023-06-12 14:50:05 +0200

    feat: change icons to be from react icons and copy clipboard change icon based on condtion

--- website/components/CodeBlock/shiki/ShikiCodeBlockClient.tsx
+++ website/components/CodeBlock/shiki/ShikiCodeBlockClient.tsx
@@ -1,13 +1,10 @@
 'use client'
 import parse from 'html-react-parser'
 
-import Image, { StaticImageData } from 'next/image'
 import { useClipboard, Button } from '@chakra-ui/react'
-
-import copyIcon from '../../../public/copyIcon.png'
-import ts from '../../../public/ts.png'
-import js from '../../../public/js.png'
-import bash from '../../../public/bash.png'
+import { MdOutlineContentCopy } from 'react-icons/md'
+import { IoMdCheckmark } from 'react-icons/io'
+import { SiJavascript, SiTypescript, SiGnubash } from 'react-icons/si'
 
 interface ShikiCodeBlockClientProps {
   html: string
@@ -24,28 +21,25 @@ export const ShikiCodeBlockClient = ({
 }: ShikiCodeBlockClientProps) => {
   const htmlCode = parse(html)
 
-  const { onCopy } = useClipboard(code)
+  const { hasCopied, onCopy } = useClipboard(code)
 
-  const icon: Record<string, StaticImageData> = {
-    JavaScript: js,
-    TypeScript: ts,
-    Bash: bash,
+  const Icon: Record<string, React.ReactNode> = {
+    JavaScript: <SiJavascript />,
+    TypeScript: <SiTypescript />,
+    Bash: <SiGnubash />,
   }
 
   return (
     <div className="bg-cat-crust border-2 rounded-lg border-cat-surface">
       <div className="flex flex-row h-10 border-b-2 border-b-cat-surface rounded-t-lg justify-between pr-4 items-center">
-        <div className="flex flex-row gap-1">
-          {icon[languageText] && (
-            <Image className="pl-2" alt="ts" src={icon[languageText]} width={30} height={20} />
-          )}
-
+        <div className="flex flex-row gap-1 ml-2 items-center">
+          {Icon[languageText] && Icon[languageText]}
           <div className="pl-4 text-sm">{filepath}</div>
         </div>
         <div className="flex flex-row gap-8">
           {languageText && <div className="text-cat-text text-sm">{languageText}</div>}
           <Button onClick={onCopy}>
-            <Image alt="copy.svg" src={copyIcon} width={20} />
+            {hasCopied ? <IoMdCheckmark /> : <MdOutlineContentCopy />}
           </Button>
         </div>
       </div>
