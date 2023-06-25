5702f138fcc0e2b259b6bbf71aeb102dac7025db
Francesco Ceccon <francesco@ceccon.me>
2023-06-25 21:29:46 +0200

    feat: remove unused block explorer code

--- packages/core/src/network.ts
+++ packages/core/src/network.ts
@@ -1,16 +1,11 @@
 import { constants } from 'starknet'
 
-export type BlockExplorerName = 'voyager'
-export type BlockExplorer = { name: string; url: string }
-
 /** Information about a Starknet network. */
 export type Chain = {
   /** Chain ID. */
   id: string
   /** Human-readable name. */
   name: string
-  /** Block explorer. */
-  blockExplorer?: BlockExplorer
   /** Flag for test networks. */
   testnet?: boolean
 }
@@ -24,26 +19,14 @@ const KNOWN_CHAINS: Record<constants.StarknetChainId, Chain> = {
   [constants.StarknetChainId.SN_MAIN]: {
     id: constants.StarknetChainId.SN_MAIN,
     name: 'Starknet Mainnet',
-    blockExplorer: {
-      name: 'Voyager',
-      url: 'https://voyager.online',
-    },
     testnet: false,
   },
   [constants.StarknetChainId.SN_GOERLI]: {
     id: constants.StarknetChainId.SN_GOERLI,
     name: 'Starknet Görli',
-    blockExplorer: {
-      name: 'Voyager',
-      url: 'https://goerli.voyager.online',
-    },
   },
   [constants.StarknetChainId.SN_GOERLI2]: {
     id: constants.StarknetChainId.SN_GOERLI2,
     name: 'Starknet Görli 2',
-    blockExplorer: {
-      name: 'Voyager',
-      url: 'https://goerli-2.voyager.online',
-    },
   },
 }
