0a2eff01e6eddff9520cc5c064caa7dc4ece8291
Francesco Ceccon <francesco@ceccon.me>
2023-07-05 13:27:42 +0200

    fix: export `useTransactionManager`

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,10 @@
-export { StarknetConfig, StarknetConfigProps, StarknetProviderProps } from './providers'
+export {
+  StarknetConfig,
+  StarknetConfigProps,
+  StarknetProviderProps,
+  useTransactionManager,
+  TransactionManagerState,
+} from './providers'
 export * from './hooks'
 export * from './connectors'
 export * from './network'
