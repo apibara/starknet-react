6eaa10803fc1aea88b52244dec878f823e16c7ea
mgavillo <marie.gavillon@gmail.com>
2023-11-07 15:49:10 +0000

    update to react-starknet v2, add tailwindcss

--- packages/create-starknet/templates/vite/src/main.tsx
+++ packages/create-starknet/templates/vite/src/main.tsx
@@ -1,17 +1,37 @@
-import { InjectedConnector, StarknetConfig } from "@starknet-react/core";
+import { StarknetConfig, argent, braavos, publicProvider, useInjectedConnectors } from "@starknet-react/core";
+import { devnet, goerli, mainnet } from "@starknet-react/chains";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./globals.css"
 
-const connectors = [
-  new InjectedConnector({ options: { id: "braavos" } }),
-  new InjectedConnector({ options: { id: "argentX" } }),
-];
+function Root({ children }: { children: React.ReactNode }) {
+  const chains = [goerli, mainnet, devnet];
+  const providers = [publicProvider()];
+  const { connectors } = useInjectedConnectors({
+    // Show these connectors if the user has no connector installed.
+    recommended: [
+      argent(),
+      braavos(),
+    ],
+    // Randomize the order of the connectors.
+    order: "random"
+  });
+
+  return (
+    <StarknetConfig autoConnect
+      chains={chains}
+      providers={providers}
+      connectors={connectors}>
+      {children}
+    </StarknetConfig>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <StarknetConfig autoConnect connectors={connectors}>
+    <Root>
       <App />
-    </StarknetConfig>
+    </Root>
   </React.StrictMode>,
 );
