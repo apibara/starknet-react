1f279ad51abbf2c398b506faa08aba66b0c46fb4
Ivan Ludvig <ivanludvigdev@gmail.com>
2023-11-07 15:40:46 +0100

    returns chain test for public provider

--- packages/core/src/providers/public.test.ts
+++ packages/core/src/providers/public.test.ts
@@ -7,4 +7,8 @@ describe("publicProvider", () => {
   it("returns a public rpc endpoint", () => {
     expect(publicProvider()(devnet)?.nodeUrl).toMatchInlineSnapshot('"http://localhost:5050/rpc"');
   });
+
+  it("returns the chain", () => {
+    expect(publicProvider()(devnet)?.getChainId()).resolves.toMatch('0x534e5f474f45524c49');
+  });
 });
