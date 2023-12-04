3249a22b5b829f0810913097dfe14cabd1e97afc
mgavillo <marie.gavillon@gmail.com>
2023-12-04 17:22:25 +0000

    change error for instance undefined

--- packages/core/src/hooks/useExplorer.ts
+++ packages/core/src/hooks/useExplorer.ts
@@ -5,6 +5,6 @@ export function useExplorer():Explorer {
     const { explorer, chain } = useStarknet()
     if (!explorer) throw Error("Explorer is undefined. Try adding it to StarknetConfig.")
     let explorerInstance = explorer(chain)
-    if (!explorerInstance) throw Error("Explorer is undefined. Try adding it to StarknetConfig.")
+    if (!explorerInstance) throw Error("Explorer Instance is undefined")
     return explorerInstance
 }
