4ee5e9481e3b24c35284dfe96c2aa5f54a9fef50
Tony Stark <oktonystark8@gmail.com>
2023-12-08 07:18:42 -0600

    feat: fonts update

---
+++ demos/companion-app/src/lib/fonts.ts
@@ -0,0 +1,8 @@
+import { GeistMono, GeistSans } from "geist/font";
+
+export type NextFont = {
+  variable: string;
+};
+
+export const sansFont: NextFont = GeistSans;
+export const monoFont: NextFont = GeistMono;
