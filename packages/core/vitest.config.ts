c73f1119a73e43f83b67060e1db29159c4074fc0
Francesco Ceccon <francesco@ceccon.me>
2023-12-28 18:23:37 +0100

    core: fix failing tests

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -3,7 +3,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths({ ignoreConfigErrors: true })],
   test: {
     globals: true,
     environment: "jsdom",
