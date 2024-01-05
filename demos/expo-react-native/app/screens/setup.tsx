e6f79e150e6843ff5ce692a754ed3455cc91a823
Tony Stark <oktonystark8@gmail.com>
2024-01-05 13:40:25 -0600

    fix: final switch

--- demos/expo-react-native/app/screens/setup.tsx
+++ demos/expo-react-native/app/screens/setup.tsx
@@ -2,6 +2,7 @@ import { H2, Button, Paragraph, YStack } from "tamagui";
 import { ChevronLeft } from "@tamagui/lucide-icons";
 import { useLink } from "solito/link";
 import { Stack } from "expo-router";
+import { Linking } from "react-native";
 
 export default function Screen() {
   return (
@@ -16,27 +17,33 @@ export default function Screen() {
   );
 }
 
+const handleOpenURL = () => {
+  Linking.openURL("https://account.starknet-react.com");
+};
+
 export function SetupScreen() {
   const link = useLink({
     href: "/",
   });
 
   return (
-    <YStack f={1} jc="flex-start" ai="center" space>
-      <YStack space="$4" bc="$background" p="$12">
+    <YStack f={1} jc="center" ai="center" space>
+      <YStack space="$4" p="$9">
         <H2 ta="center">Setup New Account</H2>
         <Button
           backgroundColor="$yellow5Light"
           color="$orange10Dark"
           variant="outlined"
+          fontSize="$5"
+          onPress={handleOpenURL}
         >
           account.starknet-react.com
         </Button>
-        <Paragraph ta="center">
+        <Paragraph ta="center" fontSize="$5">
           Visit the page following page with a web browser to setup your
           account.
         </Paragraph>
-        <Button size="$4" backgroundColor="$green5Dark" color="white">
+        <Button size="$4" bc="$orange10" fontSize="$5" color="white">
           Done
         </Button>
       </YStack>
