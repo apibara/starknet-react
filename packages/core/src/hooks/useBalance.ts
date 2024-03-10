6d65a672f9e7b27dc56334381468cb81992fb61f
notV4l <imV4l@proton.me>
2024-03-10 22:14:46 +0100

    feat: add formatUnits from viem

--- packages/core/src/hooks/useBalance.ts
+++ packages/core/src/hooks/useBalance.ts
@@ -137,9 +137,7 @@ function queryFn({
       symbolPromise,
     ]);
 
-    const formatted = (
-      Number(balanceOf * 10_000n / BigInt(10 ** decimals)) / 10_000
-    ).toString();
+    const formatted = formatUnits(balanceOf, decimals);
 
     return {
       value: balanceOf,
@@ -227,3 +225,55 @@ const balanceABIFragment = [
     type: "function",
   },
 ];
+
+
+/*
+MIT License
+
+Copyright (c) 2023-present weth, LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+https://github.com/wevm/viem/blob/main/src/utils/unit/formatUnits.ts
+*/
+
+/**
+ *  Divides a number by a given exponent of base 10 (10exponent), and formats it into a string representation of the number..
+ *
+ * - Docs: https://viem.sh/docs/utilities/formatUnits
+ *
+ * formatUnits(420000000000n, 9)
+ * // '420'
+ */
+function formatUnits(value: bigint, decimals: number) {
+  let display = value.toString()
+
+  const negative = display.startsWith('-')
+  if (negative) display = display.slice(1)
+
+  display = display.padStart(decimals, '0')
+
+  let [integer, fraction] = [
+    display.slice(0, display.length - decimals),
+    display.slice(display.length - decimals),
+  ]
+  fraction = fraction.replace(/(0+)$/, '')
+  return `${negative ? '-' : ''}${integer || '0'}${fraction ? `.${fraction}` : ''
+    }`
+}
