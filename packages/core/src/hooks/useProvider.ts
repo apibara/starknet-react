7e777511837264609a2e7cfe58eafdb721ce890e
raizo07 <agadaraizo@gmail.com>
2024-03-27 14:51:02 +0100

    update: added optional chainId to provider

--- packages/core/src/hooks/useProvider.ts
+++ packages/core/src/hooks/useProvider.ts
@@ -1,12 +1,17 @@
 import { ProviderInterface } from "starknet";
 
 import { useStarknet } from "~/context/starknet";
+import { useNetwork } from "./useNetwork";
 
 /** Value returned from `useProvider`. */
 export interface UseProviderResult {
   /** The current provider. */
   provider: ProviderInterface;
 }
+
+export type UseProviderProps = {
+  chainId?: bigint,
+};
 
 /**
  * Hook for accessing the current provider.
@@ -24,7 +29,23 @@ export interface UseProviderResult {
  * }
  * ```
  */
-export function useProvider(): UseProviderResult {
-  const { provider } = useStarknet();
-  return { provider };
+export function useProvider({ chainId }: UseProviderProps): UseProviderResult {
+  const { provider, providerFactory } = useStarknet();
+  const { chains } = useNetwork();
+
+  if (chainId === undefined) {
+    return { provider }
+  }
+
+  const chain = chains.find((ch) => ch.id === chainId);
+  if (chain === undefined) {
+    throw new Error(`Chain with id ${chainId} not found. Did you configure it?`);
+  }
+
+  const provider_ = providerFactory(chain);
+  if (provider_ === undefined || provider_ === null)  {
+    throw new Error(`No provider found for chain ${chain.name}`);
+  }
+
+  return { provider: provider_  };
 }
