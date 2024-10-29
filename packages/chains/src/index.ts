1328cb4f47bb4868566393da6d3e95c5e851bb94
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-10-30 09:58:34 +1100

    init controller WIP

--- packages/chains/src/index.ts
+++ packages/chains/src/index.ts
@@ -2,3 +2,4 @@ export type { Address, Chain, NativeCurrency, RpcUrls } from "./types";
 
 export { devnet } from "./devnet";
 export { mainnet, sepolia } from "./starknet";
+export { getSlotChain } from "./slot";
